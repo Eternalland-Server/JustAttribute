e7d76a8d437fc13357ac0857d697f01e238fdf1f
justwei <admin@taylorswiftcn.com>
2022-01-25 18:02:44 +0800

    🚧 修复绑定BUG

--- src/main/java/com/sakuragame/eternal/justattribute/core/soulbound/Action.java
+++ src/main/java/com/sakuragame/eternal/justattribute/core/soulbound/Action.java
@@ -8,11 +8,11 @@ public enum Action {
 
     SEAL(-1, false, false, new SealHandler()),
     AUTO_LOCK(0, true, true, new AutoHandler.Lock()),
-    USE_LOCK(1, true, false, new UseHandler.Lock()),
+    USE_LOCK(1, false, false, new UseHandler.Lock()),
     PROP_LOCK(2, true, true, new PropHandler.Lock()),
-    AUTO(10, false, true, new AutoHandler.Normal()),
+    AUTO(10, true, true, new AutoHandler.Normal()),
     USE(11, false, false, new UseHandler.Normal()),
-    PROP(22, false, true, new PropHandler.Normal());
+    PROP(22, true, true, new PropHandler.Normal());
 
     private final int id;
     private final boolean lock;
